19a5fd0fc3c5a9a5e5baabde10b4959c2ce23400
Marcos Vinicius <marcossilvareis.vinicios3@gmail.com>
2020-02-04 12:32:19 -0300

    Aulas_PHP

---
+++ aula04.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aula 04</title>
+</head>
+<body>
+<div>
+    <?php
+    $nome = "Marcos";
+    $idade = 16;
+
+    echo "$nome tem $idade anos!";
+
+    
+    ?>
+    </div>
+</body>
+</html>
